08ce029598c32cb357f607880a5ebff896334594
ayeolakenny <ayeolakenny@gmail.com>
2022-10-28 10:41:49 +0100

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
 const port = process.env.PORT || 4000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({
