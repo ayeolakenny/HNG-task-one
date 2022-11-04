5bc061b3e13e46f731b8dcf2107125962864d95b
ayeolakenny <ayeolakenny@gmail.com>
2022-11-04 09:31:29 +0100

    task 2

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const { json } = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 require("dotenv").config();
 
 const port = process.env.PORT || 4000;
@@ -18,6 +22,33 @@ app.get("/", (req, res) => {
     age: 21,
     bio: "My name is Ayeola Kehinide and i am a fullstack developer based in lagos",
   });
+});
+
+app.post("/", (req, res) => {
+  if (req.body.operation_type === "addition") {
+    res.json({
+      slackUsername: "ayeolakenny",
+      operation_type: req.body.operation_type,
+      result: req.body.x + req.body.y,
+    });
+  } else if (req.body.operation_type === "subtraction") {
+    res.json({
+      slackUsername: "ayeolakenny",
+      operation_type: req.body.operation_type,
+      result: req.body.x - req.body.y,
+    });
+  } else if (req.body.operation_type === "multiplication") {
+    res.json({
+      slackUsername: "ayeolakenny",
+      operation_type: req.body.operation_type,
+      result: req.body.x * req.body.y,
+    });
+  } else {
+    res.send({
+      message:
+        "operation type can only be addition, subtraction or multiplication",
+    });
+  }
 });
 
 app.listen(port, () => {
